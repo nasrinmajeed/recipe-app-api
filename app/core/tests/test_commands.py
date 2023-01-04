2f7e9b2735de4c0711062079046d8019e45d89ed
nasrinmajeed <nasrinmajeed74@gmail.com>
2023-01-04 17:51:15 +0530

    commit

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -13,6 +13,7 @@ from django.test import SimpleTestCase
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test commands."""
+
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready."""
         patched_check.return_value = True
